ac6b4f2474ddbd8bd2f661ab16b1a636cc6def40
MikaelPersson <gin10mpe@student.lu.se>
2011-11-04 11:30:45 +0100

    Productklassen är uppdaterad av Mikael

--- Product.java
+++ Product.java
@@ -8,4 +8,28 @@ public class Product {
     this.productNbr = productNbr;
     this.price = price;
   }
+ 
+ public String getName() {
+    return name;
+  }
+
+  public void setName(String name) {
+    this.name = name;
+  }
+  
+  public int getproductNbr() {
+  return productNbr;
+  }
+  
+  public void setproductNbr(int productNbr) {
+	this.productNbr = productNbr;
+  }
+  
+  public int getPrice() {
+	return price;
+  }
+  
+  piblic void setPrice(int price) {
+	this.price = price;
+  }
 }
