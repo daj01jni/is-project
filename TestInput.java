b8a45bb6e62e587a0448d431c17659edbc27e50c
John-Patrik Nilsson <daj01jni@student.lu.se>
2011-11-16 12:23:57 +0100

    example of creating new customer

--- TestInput.java
+++ TestInput.java
@@ -15,7 +15,26 @@ public class TestInput {
       in = scan.nextLine();
 
       if (in.equals("c")) {
+        Customer customer = new Customer("", "", "");
+
         System.out.println("Handlez customaorz!");
+        System.out.println("New (n), delete (d), list (l)");
+        while (!in.equals("q")) {
+          in = scan.nextLine();
+          if (in.equals("n")) {
+            // create new customer
+            System.out.println("Enter name");
+            String customerName = scan.nextLine();
+            // change the fucking name
+            customer.setName(customerName);
+          } else if (in.equals("d")) {
+            // delete customer
+          } else if (in.equals("l")) {
+            System.out.println(customer.getName());
+          } else {
+            // something else
+          }
+        }
       } else if (in.equals("p")) {
         System.out.println("Handlez producxtz!");
       } else if (in.equals("o")) {
