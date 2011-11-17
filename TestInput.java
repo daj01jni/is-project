11bacd3c382bac3349a5df4b3ebf2700b33cacf2
icastieg <gin10ekr@student.lu.se>
2011-11-17 13:42:21 +0100

    updated

--- TestInput.java
+++ TestInput.java
@@ -37,8 +37,35 @@ public class TestInput {
         }
       } else if (in.equals("p")) {
         System.out.println("Handlez producxtz!");
+        System.out.println("New(n), delete(d), list(l)");
+        in = scan.nextLine();
+          if (in.equals("n")) {
+            // create new product
+            System.out.println("Enter productname");
+            String customerProductname = scan.nextLine();
+            // change the fucking productname
+            customer.setProductname(peoductName);
+          } else if (in.equals("d")) {
+            // delete product
+          } else if (in.equals("l")) {
+            System.out.println(customer.getProductname());
+          } else {
+            // something else
+
       } else if (in.equals("o")) {
         System.out.println("Handlez ordeaaarz!");
+        System.out.println("New(n), delete(d), list(l)");
+        in = scan.nextLine();
+          if (in.equals("n")) {
+            // create new order
+            System.out.println("Enter order");
+            String Order = scan.nextLine();
+            // change the fucking order
+            customer.setOrder(customerName);
+          } else if (in.equals("d")) {
+            // delete order
+          } else if (in.equals("l")) {
+            System.out.println(customer.getOrder());
       } else {
         System.out.println("RTFM");
       }
