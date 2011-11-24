2dbb7dc40a17cc228c17e08b761d7669215c8dac
MikaelPersson <gin10mpe@student.lu.se>
2011-11-24 18:20:14 +0100

    Updated ProductReg!

---
+++ ProductRegister.java
@@ -0,0 +1,27 @@
+
+import java.util.*;
+public class ProductReg {
+  private Product product;
+private HashMap<String, Product> products = new HashMap();
+
+
+public ProductReg(){
+}
+
+public Product findProduct(String name){
+return products.get(name);
+}
+
+public void addProduct(Product product){
+products.put(product.getname(), product);  //getname(), är fel! wai?
+}
+
+public void removeproduct(String name){
+products.remove(name);
+}
+
+public Iterator iterator(){
+return products.values().iterator();
+}
+
+}
